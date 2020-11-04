8af1b9fd1a34ebcd40181908ed372d981860c7ab
Wei Zhong <ngwz92@gmail.com>
2020-11-05 00:46:01 +0800

    051120 1245

---
+++ src/firebase/firebase.js
@@ -0,0 +1,147 @@
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: process.env.FIREBASE_ENV_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added is called even for existing children, not just new ones
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     });
+//   });
+
+//   console.log(expenses);
+// });
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   });
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   amount: 109500,
+//   createdAt: 1000,
+//   note: 'This was last months rent'
+// });
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   amount: 1,
+//   createdAt: 2000,
+//   note: 'This is this months rent'
+// });
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   amount: 999,
+//   createdAt: 3000,
+//   note: 'This is next months rent'
+// });
+
+// database.ref('notes').push({
+//   title: 'first note',
+//   body: 'react notes'
+// });
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// });
+
+// setTimeout(() => {
+//   database.ref('age').set(29);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(31);
+// }, 10500);
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e);
+//   });
+
+// database.ref().set({
+//   name: 'Wei Zhong',
+//   age: 28,
+//   location: {
+//     city: 'singapore',
+//     country: 'singapore'
+//   },
+//   job: {
+//     title: 'software developer',
+//     company: 'google'
+//   },
+//   stressLevel: 6
+// }).then(() => {
+//   console.log('data is saved');
+// }).catch((e) => {
+//   console.log('this failed', e);
+// });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'amazon',
+//   'location/city': 'seattle'
+// }).then(() => {
+//   console.log('data is updated');
+// }).catch((e) => {
+//   console.log('data update failed', e);
+// });
+
+// database.ref()
+//   .remove()
+//   .then(() => {
+//     'age removed'
+//   })
+//   .catch((e) => {
+//     console.log('age removal failed', e);
+//   });
